7ba131457f0ad58cadcd0ecce37c17118b24a6d1
javierrta <castraining2021@gmail.com>
2021-10-20 12:07:42 +0200

    "2021-10_10 Corregida BBDD"

--- controladores/configuracion_piensasolutions.php
+++ controladores/configuracion_piensasolutions.php
@@ -1,5 +1,5 @@
 <?php
 
-define("SERVERBD", "mysql:host=my0370.20663profesor.com; dbname=my0370");
-define("USERBD", "my0370");
+define("SERVERBD", "mysql:host=qafs509.20663profesor.com; dbname=qafs509");
+define("USERBD", "qafs509");
 define("PASSBD", "PSget6389");
