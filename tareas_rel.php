96148faa24b83b9e4179fadf505da9287dd96eae
JorgeRsptn <59470958+JorgeRsptn@users.noreply.github.com>
2021-10-20 18:10:03 +0200

    tareas_rel delete y reenvio a form para update

--- tareas_rel.php
+++ tareas_rel.php
@@ -14,6 +14,15 @@ if (!isset($_SESSION['id'])) {
     $datos = json_decode($response);
 
     $modificar = null;
+
+    if (isset($_GET['modificar'])) {
+        $modificar = true;
+
+        $sql = "SELECT tar_id, tar_nombre, tar_fr_inicio, tar_fr_fin, tar_ft_inicio, tar_ft_fin, tar_usu_id, tar_duracion, tar_sit_id, tar_acc_id, tar_obs FROM tareas where tar_usu_id = 5 AND tar_acc_id AND tar_id = $modificar";
+        $response = controlador::select($sql);
+        $datos = json_decode($response);
+    }
+
 }
 
 ?>
@@ -31,130 +40,132 @@ if (!isset($_SESSION['id'])) {
     <h1>Relación de tareas del proyecto <?php echo 'id de acción'//$accionId ?></h1>
 </header>
 <main>
-    <table style="border: black 1px solid; ">
-        <th>
-        <td>
-            id de tarea
-        </td>
-        <td>
-            nombre de tarea
-        </td>
 
-        <td>
-            F. real inicio
-        </td>
+    <?php if ($modificar) : ?>
+        <form method="get" action="">
+            <input type="hidden" name="tar_id" value="">
+            <label for="tar_nombre">
+                <input type="text" name="tar_nombre" id=""> <?php $datos->tar_nombre  ?>
+            </label>
+            <label for="tar_fr_inicio">
+                <input type="text" name="tar_fr_inicio" id="">
+            </label>
+            <label for="tar_fr_fin">
+                <input type="text" name="tar_fr_fin" id="">
+            </label>
+            <label for="tar_ft_inicio">
+                <input type="text" name="tar_ft_inicio" id="">
+            </label>
+            <label for="tar_ft_fin">
+                <input type="text" name="tar_ft_fin" id="">
+            </label>
+            <label for="tar_usu_id">
+                <input type="text" name="tar_usu_id" id="">
+            </label>
+            <label for="tar_duracion">
+                <input type="text" name="tar_duracion" id="">
+            </label>
+            <label for="tar_sit_id">
+                <input type="text" name="tar_sit_id" id="">
+            </label>
+            <label for="tar_acc_id">
+                <input type="text" name="tar_acc_id" id="">
+            </label>
+            <label for="tar_obs">
+                <input type="text" name="tar_obs" id="">
+            </label>
+            <button action="" method="POST" name="updateTarea">Modificar</button>
+        </form>
+    <?php else: ?>
+        <table style="border: black 1px solid; ">
+            <th>
+            <td>
+                id de tarea
+            </td>
+            <td>
+                nombre de tarea
+            </td>
 
-        <td>
-            F. real fin
-        </td>
+            <td>
+                F. real inicio
+            </td>
 
-        <td>
-            F. teórica inicio
-        </td>
-        <td>
-            F. teórica fin
-        </td>
-        <td>
-            id de usuario
-        </td>
-        <td>
-            duración
-        </td>
-        <td>
-            situación
-        </td>
-        <td>
-            id de acción
-        </td>
-        <td>
-            observaciones
-        </td>
-        </th>
-        <?php if ($modificar != null) : ?>
-            <form>
-                <input type="hidden" name="tar_id" value="">
-                <label for="tar_nombre">
-                    <input type="text" name="tar_nombre" id="">
-                </label>
-                <label for="tar_fr_inicio">
-                    <input type="text" name="tar_fr_inicio" id="">
-                </label>
-                <label for="tar_fr_fin">
-                    <input type="text" name="tar_fr_fin" id="">
-                </label>
-                <label for="tar_ft_inicio">
-                    <input type="text" name="tar_ft_inicio" id="">
-                </label>
-                <label for="tar_ft_fin">
-                    <input type="text" name="tar_ft_fin" id="">
-                </label>
-                <label for="tar_usu_id">
-                    <input type="text" name="tar_usu_id" id="">
-                </label>
-                <label for="tar_duracion">
-                    <input type="text" name="tar_duracion" id="">
-                </label>
-                <label for="tar_sit_id">
-                    <input type="text" name="tar_sit_id" id="">
-                </label>
-                <label for="tar_acc_id">
-                    <input type="text" name="tar_acc_id" id="">
-                </label>
-                <label for="tar_obs">
-                    <input type="text" name="tar_obs" id="">
-                </label>
-            </form>
-        <?php else: ?>
+            <td>
+                F. real fin
+            </td>
 
-        <?php foreach ($datos as $registro) : ?>
-            <tr style="border: blue 1px solid; ">
-                <td style="border: greenyellow 1px solid; ">
-                    <?php echo($registro->tar_id) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_nombre) ?>
-                </td>
-
-                <td>
-                    <?php echo($registro->tar_fr_inicio) ?>
-                </td>
-
-                <td>
-                    <?php echo($registro->tar_fr_fin) ?>
-                </td>
-
-                <td>
-                    <?php echo($registro->tar_ft_inicio) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_ft_fin) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_usu_id) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_duracion) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_sit_id) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_acc_id) ?>
-                </td>
-                <td>
-                    <?php echo($registro->tar_obs) ?>
-                </td>
-                <?php if ($usuId == $usuId) : ?>
-                    <td>
-                        <button value="<?php echo($registro->tar_obs) ?> ">Modificar</button>
+            <td>
+                F. teórica inicio
+            </td>
+            <td>
+                F. teórica fin
+            </td>
+            <td>
+                id de usuario
+            </td>
+            <td>
+                duración
+            </td>
+            <td>
+                situación
+            </td>
+            <td>
+                id de acción
+            </td>
+            <td>
+                observaciones
+            </td>
+            </th>
+            <?php foreach ($datos as $registro) : ?>
+                <tr style="border: blue 1px solid; ">
+                    <td style="border: greenyellow 1px solid; ">
+                        <?php echo($registro->tar_id) ?>
                     </td>
                     <td>
-                        <button value="<?php echo($registro->tar_obs) ?>">Borrar</button>
+                        <?php echo($registro->tar_nombre) ?>
                     </td>
-                <?php endif; ?>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+
+                    <td>
+                        <?php echo($registro->tar_fr_inicio) ?>
+                    </td>
+
+                    <td>
+                        <?php echo($registro->tar_fr_fin) ?>
+                    </td>
+
+                    <td>
+                        <?php echo($registro->tar_ft_inicio) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_ft_fin) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_usu_id) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_duracion) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_sit_id) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_acc_id) ?>
+                    </td>
+                    <td>
+                        <?php echo($registro->tar_obs) ?>
+                    </td>
+                    <?php if ($usuId == $usuId) : ?>
+                        <td>
+                            <button name="modificar" value="<?php echo($registro->tar_id) ?>" action="">Modificar
+                            </button>
+                        </td>
+                        <td>
+                            <button value="<?php echo($registro->tar_obs) ?>">Borrar</button>
+                        </td>
+                    <?php endif; ?>
+                </tr>
+            <?php endforeach; ?>
+        </table>
     <?php endif; ?>
 </main>
 
