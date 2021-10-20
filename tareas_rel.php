9ce83f8fa31818a3ede3566050f3d76e1b357f0b
JorgeRsptn <59470958+JorgeRsptn@users.noreply.github.com>
2021-10-20 18:30:16 +0200

    Sentencia multitabla

--- tareas_rel.php
+++ tareas_rel.php
@@ -8,27 +8,30 @@ if (!isset($_SESSION['id'])) {
     $usuId = 5;
 //$accionId = $_REQUEST['accionId'];
 //echo $usuId;
-    //$sql = "SELECT * FROM tareas where tar_usu_id = $usuId AND tar_acc_id = 1";
-    $sql = "SELECT tar_id, tar_nombre, tar_fr_inicio, tar_fr_fin, tar_ft_inicio, tar_ft_fin, tar_usu_id, tar_duracion, tar_sit_id, tar_acc_id, tar_obs FROM tareas where tar_usu_id = 5 AND tar_acc_id = 1";
+
+
+    if (isset($_POST['idTarea'])) {
+        $idTarea = $_POST['idTarea'];
+        $sql = "DELETE FROM `tareas` WHERE tar_id = $idTarea";
+        $response = controlador::delete($sql);
+    }
+
+    $sql = "SELECT tar_id, tar_nombre, tar_fr_inicio, tar_fr_fin, tar_ft_inicio, tar_ft_fin, tar_usu_id, u.usu_nombre,
+                    tar_duracion, s.sit_nombre as tar_sit_id, tar_acc_id, tar_obs
+            FROM tareas 
+            INNER JOIN situaciones s on tareas.tar_sit_id = s.sit_id 
+            INNER JOIN usuarios u on tareas.tar_usu_id = u.usu_id
+            WHERE tar_usu_id = 11 AND tar_acc_id = 1";
     $response = controlador::select($sql);
     $datos = json_decode($response);
 
-    $modificar = null;
-
-    if (isset($_GET['modificar'])) {
-        $modificar = true;
-
-        $sql = "SELECT tar_id, tar_nombre, tar_fr_inicio, tar_fr_fin, tar_ft_inicio, tar_ft_fin, tar_usu_id, tar_duracion, tar_sit_id, tar_acc_id, tar_obs FROM tareas where tar_usu_id = 5 AND tar_acc_id AND tar_id = $modificar";
-        $response = controlador::select($sql);
-        $datos = json_decode($response);
-    }
 
 }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="esES">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -40,133 +43,102 @@ if (!isset($_SESSION['id'])) {
     <h1>Relación de tareas del proyecto <?php echo 'id de acción'//$accionId ?></h1>
 </header>
 <main>
+    <table style="border: black 1px solid; ">
+        <th>
+        <td>
+            id de tarea
+        </td>
+        <td>
+            nombre de tarea
+        </td>
 
-    <?php if ($modificar) : ?>
-        <form method="get" action="">
-            <input type="hidden" name="tar_id" value="">
-            <label for="tar_nombre">
-                <input type="text" name="tar_nombre" id=""> <?php $datos->tar_nombre  ?>
-            </label>
-            <label for="tar_fr_inicio">
-                <input type="text" name="tar_fr_inicio" id="">
-            </label>
-            <label for="tar_fr_fin">
-                <input type="text" name="tar_fr_fin" id="">
-            </label>
-            <label for="tar_ft_inicio">
-                <input type="text" name="tar_ft_inicio" id="">
-            </label>
-            <label for="tar_ft_fin">
-                <input type="text" name="tar_ft_fin" id="">
-            </label>
-            <label for="tar_usu_id">
-                <input type="text" name="tar_usu_id" id="">
-            </label>
-            <label for="tar_duracion">
-                <input type="text" name="tar_duracion" id="">
-            </label>
-            <label for="tar_sit_id">
-                <input type="text" name="tar_sit_id" id="">
-            </label>
-            <label for="tar_acc_id">
-                <input type="text" name="tar_acc_id" id="">
-            </label>
-            <label for="tar_obs">
-                <input type="text" name="tar_obs" id="">
-            </label>
-            <button action="" method="POST" name="updateTarea">Modificar</button>
-        </form>
-    <?php else: ?>
-        <table style="border: black 1px solid; ">
-            <th>
-            <td>
-                id de tarea
-            </td>
-            <td>
-                nombre de tarea
-            </td>
+        <td>
+            F. real inicio
+        </td>
 
-            <td>
-                F. real inicio
-            </td>
+        <td>
+            F. real fin
+        </td>
 
-            <td>
-                F. real fin
-            </td>
+        <td>
+            F. teórica inicio
+        </td>
+        <td>
+            F. teórica fin
+        </td>
+        <td>
+            id de usuario
+        </td>
+        <td>
+            duración
+        </td>
+        <td>
+            situación
+        </td>
+        <td>
+            id de acción
+        </td>
+        <td>
+            observaciones
+        </td>
+        </th>
+        <?php foreach ($datos as $registro) : ?>
+            <tr style="border: blue 1px solid; ">
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_id) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_nombre) ?>
+                </td>
 
-            <td>
-                F. teórica inicio
-            </td>
-            <td>
-                F. teórica fin
-            </td>
-            <td>
-                id de usuario
-            </td>
-            <td>
-                duración
-            </td>
-            <td>
-                situación
-            </td>
-            <td>
-                id de acción
-            </td>
-            <td>
-                observaciones
-            </td>
-            </th>
-            <?php foreach ($datos as $registro) : ?>
-                <tr style="border: blue 1px solid; ">
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_fr_inicio) ?>
+                </td style="border: greenyellow 1px solid; ">
+
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_fr_fin) ?>
+                </td>
+
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_ft_inicio) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_ft_fin) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_usu_id) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_duracion) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_sit_id) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_acc_id) ?>
+                </td>
+                <td style="border: greenyellow 1px solid; ">
+                    <?php echo($registro->tar_obs) ?>
+                </td>
+                <?php if ($usuId == $usuId) : ?>
                     <td style="border: greenyellow 1px solid; ">
-                        <?php echo($registro->tar_id) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_nombre) ?>
-                    </td>
-
-                    <td>
-                        <?php echo($registro->tar_fr_inicio) ?>
-                    </td>
-
-                    <td>
-                        <?php echo($registro->tar_fr_fin) ?>
-                    </td>
-
-                    <td>
-                        <?php echo($registro->tar_ft_inicio) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_ft_fin) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_usu_id) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_duracion) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_sit_id) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_acc_id) ?>
-                    </td>
-                    <td>
-                        <?php echo($registro->tar_obs) ?>
-                    </td>
-                    <?php if ($usuId == $usuId) : ?>
-                        <td>
-                            <button name="modificar" value="<?php echo($registro->tar_id) ?>" action="">Modificar
+                        <form action="tareas_frm.php" method="POST">
+                            <input type="hidden" name="idTarea" value="<?php echo($registro->tar_id)?>">
+                            <button name="modificar" value="<?php echo($registro->tar_id) ?>">Modificar
                             </button>
-                        </td>
-                        <td>
-                            <button value="<?php echo($registro->tar_obs) ?>">Borrar</button>
-                        </td>
-                    <?php endif; ?>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-    <?php endif; ?>
+                        </form>
+                    </td >
+                    <td style="border: greenyellow 1px solid; ">
+                        <form action="" method="POST">
+                            <input type="hidden" name="idTarea" value="<?php echo($registro->tar_id) ?>">
+                            <button name="borrar">Borrar
+                            </button>
+                        </form>
+                    </td>
+                <?php endif; ?>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </main>
 
 </body>
