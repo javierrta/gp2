147c13d6843153e3d8c13542817ebc3d6f0d2767
JorgeRsptn <59470958+JorgeRsptn@users.noreply.github.com>
2021-10-20 10:16:35 +0200

    Configuracion conexion con pdo

---
+++ controladores/conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+//require_once("config.php");
+//require_once('localConfig.php');
+
+class Conexion
+{
+
+    public static function conectar()
+    {
+
+        try {
+            // Crear una conexión instanciando un objeto PDO y seteándolo
+            //$PDO = new PDO(SERVERBD, USERBD, PASSBD);
+            $PDO = new PDO('mysql:host=localhost; dbname=bd_proyectos', 'root', "");
+            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $PDO->exec("SET CHARACTER SET utf8");
+
+            return $PDO;
+        } catch (Exception $e) {
+            die('Error: ' . $e->getMessage());
+        } finally {
+            $PDO = null;
+        }
+    }
+}
---
+++ controladores/controlador.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once ('conexion.php');
+
+//Controlador que recibe los sql y devuelve Json.
