7d00fdc621cf62976b3652245abe38f158a877db
javierrta <castraining2021@gmail.com>
2021-10-21 06:09:45 +0200

    "Rama Javier"

--- prueba.php
+++ prueba.php
@@ -3,7 +3,10 @@ require_once("controladores/controlador.php");
 
 $sql = "SELECT * FROM situaciones";
 echo $sql."<br>";
+
 $datos = controlador::select($sql);
 echo '<pre>';
 var_export(json_decode($datos));
 echo '</pre>';
+
+echo "cambiado en rama";
